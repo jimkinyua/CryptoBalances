6649b89e9c1ec3d6d94f1d535ddf75f22afa8758
JimKinyua <jimkinyua25@gmail.com>
2022-09-06 15:57:28 +0300

    Feat: Filter when provided with token

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-
-const args = require('yargs').argv;
+const yargs = require('yargs');
 const Reader = require('readline');
 const fetch = require('node-fetch');
 
@@ -11,16 +10,22 @@ const apiVariables ={
 
 var TokenUSDValues;
 
+parameters = yargs.argv;
 
-var get_Latest_USD_Portifolio_Value_For_Each_Token = function () {
-        
+let get_Latest_USD_Portifolio_Value_For_Each_Token =(token) => {
+        // console.log(token)
         var PortifolioValues = [];
 
-        //Default Values Fot Our Tokens.
-        var BitCoinTransactions = { "token": "BTC", "amount": 0, "timestamp": 0 };
-        var EheriumTransactions = { "token": "ETH", "amount": 0, "timestamp": 0 };
-        var XRPTransactions = { "token": "XRP", "amount": 0, "timestamp": 0 };
+        // if(token){ //Check If Toke Provided
+            let SelectedTokenTransactions = { "token": token, "amount": 0, "timestamp": 0 };
+        // }else{
+            //No Token Not Provided Hence Default Values Fot Our Tokens are these.
+            let BitCoinTransactions = { "token": "BTC", "amount": 0, "timestamp": 0 };
+            let EheriumTransactions = { "token": "ETH", "amount": 0, "timestamp": 0 };
+            let XRPTransactions = { "token": "XRP", "amount": 0, "timestamp": 0 };
+        // }
 
+       
         var lineReader = Reader.createInterface({
             input: require('fs').createReadStream('E:\\Propine\\test.csv')
         });
@@ -36,46 +41,64 @@ var get_Latest_USD_Portifolio_Value_For_Each_Token = function () {
             LineData.amount = SplitLineDataArray[3];
 
             // console.log(LineData);
-            
-            if (LineData.token === 'BTC') {
-                //Check if Current Line Timestamp is Greater than What is in Our BitCoinTransactions Object
-                if (LineData.timestamp > BitCoinTransactions.timestamp) {   
-                    BitCoinTransactions.amount = LineData.amount;
-                    BitCoinTransactions.timestamp = LineData.timestamp;
+            if(token && LineData.token == token){ //Check If Toke Provided
+                if (LineData.timestamp > SelectedTokenTransactions.timestamp) { 
+                    SelectedTokenTransactions.amount = LineData.amount;
+                    SelectedTokenTransactions.timestamp = LineData.timestamp;
+                }
+            }else{//No Token Provided
+                if (LineData.token === 'BTC') {
+                    //Check if Current Line Timestamp is Greater than What is in Our BitCoinTransactions Object
+                    if (LineData.timestamp > BitCoinTransactions.timestamp) {   
+                        BitCoinTransactions.amount = LineData.amount;
+                        BitCoinTransactions.timestamp = LineData.timestamp;
+                    }
+                }
+                else if (LineData.token === 'ETH') {
+                    //Check if Current Line Timestamp is Greater than What is in Our EheriumTransactions Object
+                    if (LineData.timestamp > EheriumTransactions.timestamp) {
+                        EheriumTransactions.amount = LineData.amount;
+                        EheriumTransactions.timestamp = LineData.timestamp
+                    }
+                }
+                else if (LineData.token === 'XRP') {
+                    //Check if Current Line Timestamp is Greater than What is in Our XRPTransactions Object
+                    if (LineData.timestamp > XRPTransactions.timestamp) {
+                        XRPTransactions.amount = LineData.amount;
+                        XRPTransactions.timestamp = LineData.timestamp;
+                    }
                 }
             }
-            else if (LineData.token === 'ETH') {
-                //Check if Current Line Timestamp is Greater than What is in Our EheriumTransactions Object
-                if (LineData.timestamp > EheriumTransactions.timestamp) {
-                    EheriumTransactions.amount = LineData.amount;
-                    EheriumTransactions.timestamp = LineData.timestamp
-                }
-            }
-            else if (LineData.token === 'XRP') {
-                //Check if Current Line Timestamp is Greater than What is in Our XRPTransactions Object
-                if (LineData.timestamp > XRPTransactions.timestamp) {
-                    XRPTransactions.amount = LineData.amount;
-                    XRPTransactions.timestamp = LineData.timestamp;
-                }
-            }
+           
         }
 
         );
-        lineReader.on('close', function (line) {
+        lineReader.on('close', function () {
 
-            let ApiResult = get_All_Tokens_USD_Values();
+            if(token && SelectedTokenTransactions.timestamp <= 0){ //No Toke Found Hence no need to make the API call
+                return console.log('No Coin Found');
+            }
+
+            let ApiResult = get_All_Tokens_USD_Values(token);
 
             ApiResult.then( (response)=> {
                 response.json().then((USD_Values)=>{
-                    //Do the Math to Covert to USD
-                EheriumTransactions.amount = EheriumTransactions.amount * USD_Values.ETH.USD;
-                BitCoinTransactions.amount = BitCoinTransactions.amount * USD_Values.BTC.USD;
-                XRPTransactions.amount = XRPTransactions.amount * USD_Values.XRP.USD;
 
-                // console.log(XRPTransactions);
-                PortifolioValues.push(EheriumTransactions);
-                PortifolioValues.push(BitCoinTransactions);
-                PortifolioValues.push(XRPTransactions);
+                    if(token){
+                        // console.log(USD_Values);
+                        SelectedTokenTransactions.amount = SelectedTokenTransactions.amount * USD_Values.USD;
+                        PortifolioValues.push(SelectedTokenTransactions);
+                    }else{
+                            //Do the Math to Covert to USD
+                            EheriumTransactions.amount = EheriumTransactions.amount * USD_Values.ETH.USD;
+                            BitCoinTransactions.amount = BitCoinTransactions.amount * USD_Values.BTC.USD;
+                            XRPTransactions.amount = XRPTransactions.amount * USD_Values.XRP.USD;
+
+                            // console.log(XRPTransactions);
+                            PortifolioValues.push(EheriumTransactions);
+                            PortifolioValues.push(BitCoinTransactions);
+                            PortifolioValues.push(XRPTransactions);
+                    }
                 
                 //Log the Latest Portifolio Values to the User
                 console.table(PortifolioValues)
@@ -86,11 +109,19 @@ var get_Latest_USD_Portifolio_Value_For_Each_Token = function () {
 }
 
 // function to fetch the USD Values from CryptoCompare
-let get_All_Tokens_USD_Values = ()=> {
-    var cryptoURL = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,XRP,ETH&tsyms=USD&api_key=${apiVariables.token}`;
+let get_All_Tokens_USD_Values = (token)=> {
+    if(token){
+        var cryptoURL = `https://min-api.cryptocompare.com/data/price?fsym=${token}&tsyms=USD&api_key=${apiVariables.token}`;
+    }else{
+        var cryptoURL = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,XRP,ETH&tsyms=USD&api_key=${apiVariables.token}`;
+    }
     return fetch(cryptoURL)
 }
 
-if(args.token === undefined && args.date === undefined){
-    get_Latest_USD_Portifolio_Value_For_Each_Token();
+// if(args.token === undefined && args.date === undefined){
+    // get_Latest_USD_Portifolio_Value_For_Each_Token();
+// }else 
+if (parameters.token){
+    // console.log(parameters.token)
+    get_Latest_USD_Portifolio_Value_For_Each_Token(parameters.token);
 }
